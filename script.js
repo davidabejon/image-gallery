9e915431e715be907b32eb4b3a221df081bb5c9b
David Abejón <davidabejonheras@gmail.com>
2023-03-08 22:39:31 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,80 @@
+const images = document.getElementsByClassName("image");
+const zoomedImages = document.getElementsByClassName("zoomed");
+const down = document.getElementById("down");
+
+let index = 0;
+let nextImage = images[0];
+let lastCoords = { x: 0, y: 0 };
+
+const activate = (image, x, y) => {
+  image.style.left = `${x}px`;
+  image.style.top = `${y}px`;
+  image.style.zIndex = index;
+
+  image.dataset.status = "active";
+
+  lastCoords = { x, y };
+}
+
+const distanceFromLast = (x, y) => {
+  return Math.hypot(x - lastCoords.x, y - lastCoords.y);
+}
+
+let zoomed = false;
+
+const handleOnMove = e => {
+
+    if (!zoomed) {
+        if(distanceFromLast(e.clientX, e.clientY) > 200) {
+            nextImage = images[index % images.length];
+            const lastImage = images[(index - 5) % images.length];
+    
+        activate(nextImage, e.clientX, e.clientY);
+    
+        if(lastImage) lastImage.dataset.status = "inactive";
+        
+        index++;
+    }
+  }
+}
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
+
+function Position(obj){
+    var currenttop = 0;
+    if (obj.offsetParent){
+        do {
+            currenttop += obj.offsetTop;
+        } while ((obj = obj.offsetParent));
+        return [currenttop];
+    }
+   }
+
+function goDown() {
+    window.scroll(0, Position(document.getElementById("down")))
+}
+
+window.onclick = e => {
+    zoomed = !zoomed;
+
+    for (let i = 0; i < images.length; i++) {
+        images[i].classList.toggle("fade");
+    }
+
+    for (let i = 0; i < zoomedImages.length; i++) {
+        if (zoomedImages[i].dataset.index == nextImage.dataset.index) {
+            zoomedImages[i].classList.add("visible");
+        } else {
+            zoomedImages[i].classList.remove("visible");
+        }
+    }
+
+    if (zoomed) {
+        setTimeout(goDown, 1000);
+    } else {
+        window.scroll(0, 0);
+    }
+    
+};
